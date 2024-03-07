2ea140b597ca3ecb628b8f9784b6303329476fb1
KY _ <73789857+kapten-kaizo@users.noreply.github.com>
2024-03-07 19:54:52 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(
-  name = 'pyshell',         # How you named your package folder (MyLib)
-  packages = ['pyshell'],   # Chose the same as "name"
+  name = 'captpyshell',         # How you named your package folder (MyLib)
+  packages = ['captpyshell'],   # Chose the same as "name"
   version = '1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'python debugger, inject code into python prosses',   # Give a short description about your library
